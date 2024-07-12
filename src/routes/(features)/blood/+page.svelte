f1e9cdd19485a05da96ec5857d1e7b17dbafe04c
heronet <sirat4757@gmail.com>
2024-07-12 20:42:22 +0600

    add: blood home

---
+++ src/routes/(features)/blood/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<main class="mx-auto max-w-screen-xl p-4">
+	<div class="my-24">
+		<p class="text-center text-6xl font-bold">Be a Lifesaver: Donate Blood, Save Lives</p>
+		<p class="my-4 py-2 text-center">
+			Your donation has the power to be the difference between life and death. Donating blood is a
+			quick, safe, and easy way to be a hero. Just one donation can save up to three lives!
+		</p>
+		<div class="flex justify-center gap-4">
+			<Button>Donate Blood</Button>
+			<Button variant="destructive">Ask for help</Button>
+		</div>
+	</div>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,22 @@
-<main class="max-w-screen-xl p-4">
+<main class="max-w-screen-xl p-4 mx-auto">
 	<div class="my-24">
 		<p class="text-6xl font-bold text-center">The world needs You</p>
 		<p class="text-center">Help someone survive with your help.</p>
 	</div>
-	<div class="flex flex-col gap-4">
-		<div class="bg-primary text-primary-foreground p-4 px-8 rounded-r-full w-[60%]">
+	<div>
+		<a
+			href="/blood"
+			class="float-left bg-primary text-primary-foreground py-6 px-8 rounded-r-full w-[60%]"
+		>
 			<p class="font-bold text-3xl">Exchange blood</p>
 			<p class="text-muted-foreground">Donate blood to save lives, or seek help from donors</p>
-		</div>
-		<div
-			class="bg-primary justify-self-end text-primary-foreground p-4 px-8 rounded-l-full w-[60%]"
+		</a>
+		<a
+			href="/flood"
+			class="my-4 float-right bg-primary justify-self-end text-primary-foreground py-6 px-8 rounded-l-full w-[60%]"
 		>
 			<p class="font-bold text-3xl">Help Flooded locations</p>
 			<p class="text-muted-foreground">Donate blood to save lives, or seek help from donors</p>
-		</div>
+		</a>
 	</div>
 </main>
